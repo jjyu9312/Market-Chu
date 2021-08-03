60f2c819edc0e5bb9229d94ded460771436596b0
kkw <byl9312@gmail.com>
2021-08-03 15:30:40 +0900

    first commit

---
+++ src/test/java/com/toy/chustnut/ChustnutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.chustnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChustnutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
